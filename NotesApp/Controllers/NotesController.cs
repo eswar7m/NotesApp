75d1b874668a171a949f2657a497f1a2897ee5d8
Eswar <meejurueswar9@gmail.com>
2026-01-20 08:08:52 +0530

    Add project files.

---
+++ NotesApp/Controllers/NotesController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NotesApp.Data;
+using NotesApp.Models;
+
+namespace NotesApp.Controllers
+{
+    public class NotesController : Controller
+    {
+        private readonly ILogger<NotesController> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public NotesController(ILogger<NotesController> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Notes()
+        {
+            List<Note> allNotes = _context.Notes.ToList();
+            return View(allNotes);
+        }
+
+        public IActionResult CreateEditNote(int? id)
+        {
+            Note? noteToEdit = null;
+            if (id != null)
+            {
+                noteToEdit = _context.Notes.FirstOrDefault(note => note.Id == id);
+            }
+            return View(noteToEdit);
+        }
+
+        [HttpPost]
+        public IActionResult CreateEditNote(Note note)
+        {
+            if (note.Id == 0)
+            {
+                _context.Notes.Add(note);
+            }
+            else
+            {
+                _context.Notes.Update(note);
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Notes");
+        }
+
+        public IActionResult DeleteNote(int id)
+        {
+            Note? noteToDelete = _context.Notes.FirstOrDefault(note => note.Id == id);
+            if (noteToDelete != null)
+            {
+                _context.Notes.Remove(noteToDelete);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Notes");
+        }
+    }
+}
---
+++ NotesApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotesApp.Models;
+
+namespace NotesApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Note> Notes { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
